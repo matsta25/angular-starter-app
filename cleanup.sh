5c7035cae2d0ba0ddd5d8483dc4a8b1348b850ea
matsta25 <matim2112@gmail.com>
2020-03-29 10:14:03 +0200

    feat(cleanup): update cleanup.sh script.

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC2120
 
-### This script will rename your angular project.
+### This script will rename your angular project. ~matsta25
 
-old_app_name="angular-starter-app"
+set -o nounset
+set -o errexit
+
+old_app_name="asd"
 new_app_name=$1
 
 has_not_arg() {
@@ -15,9 +18,12 @@ has_not_arg() {
 }
 
 has_arg() {
-  find ./ -not -path "./node_modules/*" -type f -exec sed -i "s/$old_app_name/$new_app_name/g" {} + &&
-    echo "# $new_app_name" >README.md &&
-    rm -rf ./docs &&
+  echo "Finding and replacing from $old_app_name to $new_app_name..." &&
+    find ./ -not -path "./node_modules/*" -type f -exec sed -i "s/$old_app_name/$new_app_name/g" {} + &&
+    echo "Overwriting README.md file..." && echo "# $new_app_name" >README.md &&
+    echo "Removing docs directory..." && rm -rf ./docs &&
+    echo "Removing .gitlab-ci.yml..." && rm .gitlab-ci.yml &&
+    echo "Removing .travis.yml..." && rm .travis.yml &&
     echo &&
     echo "Project successfully cleaned from $old_app_name to $new_app_name." &&
     echo &&
