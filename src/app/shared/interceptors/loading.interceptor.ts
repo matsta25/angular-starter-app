c0fc17bcdb4f1950acecfbc0b5f3982eadc1b012
matsta25 <matim2112@gmail.com>
2020-04-05 23:27:21 +0200

    feat(loading): add working loading arr to shared module v2

--- src/app/shared/interceptors/loading.interceptor.ts
+++ src/app/shared/interceptors/loading.interceptor.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core'
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { select, Store } from '@ngrx/store'
+import { Store } from '@ngrx/store'
 import { SharedState } from '../store/shared.state'
-import { selectLoading } from '../store/shared.selectors'
 import { loadingOff, loadingOn } from '../store/shared.actions'
 import { finalize } from 'rxjs/operators'
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-  private loading$: Observable<boolean>
 
   constructor(private store: Store<SharedState>) {
-    this.loading$ = store.pipe(select(selectLoading))
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
