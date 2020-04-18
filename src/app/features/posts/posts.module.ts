84851447a1c64b509c7815e28556ca92203704c3
cichy380 <marcin.dobroszek@gmail.com>
2020-04-18 20:39:46 +0200

    feat(posts) Add PostDetailsComponent

--- src/app/features/posts/posts.module.ts
+++ src/app/features/posts/posts.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { PostsRoutingModule } from './posts-routing.module'
+import { SharedModule } from '../../shared/shared.module'
 import { PostsComponent } from './posts.component'
 import { PostsListComponent } from './components/posts-list/posts-list.component'
+import { PostDetailsComponent } from './components/post-details/post-details.component'
 import { PostCreateComponent } from './components/post-create/post-create.component'
-import { SharedModule } from '../../shared/shared.module'
 import { PostUpdateComponent } from './components/post-update/post-update.component'
-import { ReactiveFormsModule } from '@angular/forms'
 
 
 @NgModule({
   declarations: [
     PostsComponent,
     PostsListComponent,
+    PostDetailsComponent,
     PostCreateComponent,
     PostUpdateComponent,
   ],
