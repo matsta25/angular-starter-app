4528f58054b83c25ab7ad885ceac5a4f70525d30
cichy380 <marcin.dobroszek@gmail.com>
2020-05-02 18:56:43 +0200

    feat(AppComponent) Add toggleable mat-sidenav element

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Output } from '@angular/core'
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core'
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() sidenavIconClick = new EventEmitter()
 }
