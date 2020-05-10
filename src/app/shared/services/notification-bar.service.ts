e0a6a00512c847afaa7000d87c9723f4eaf225a9
cichy380 <marcin.dobroszek@gmail.com>
2020-05-10 20:51:38 +0200

    refactor(Notification) Add show() method (optimize)

--- src/app/shared/services/notification-bar.service.ts
+++ src/app/shared/services/notification-bar.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core'
-import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar'
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'
 import { NotificationBarComponent } from '../components/notification-bar/notification-bar.component'
 import { defaultSnackBarConfig, errorSnackBarConfig, successSnackBarConfig } from '../models/snackbar.model'
 
@@ -12,35 +12,19 @@ export class NotificationBarService {
   }
 
   public showInfo(message: string, duration?: number): MatSnackBarRef<SimpleSnackBar> {
-    const snackBarConfig = duration ? {...defaultSnackBarConfig, duration} : defaultSnackBarConfig
-    let ref
-
-    if (message) {
-      this.zone.run(() => ref = this.snackBar.openFromComponent(NotificationBarComponent, {
-        ...snackBarConfig,
-        data: {text: message},
-      }))
-    }
-
-    return ref
+    return this.show(defaultSnackBarConfig, message, duration)
   }
 
   public showSuccess(message: string, duration?: number): MatSnackBarRef<SimpleSnackBar> {
-    const snackBarConfig = duration ? {...successSnackBarConfig, duration} : successSnackBarConfig
-    let ref
-
-    if (message) {
-      this.zone.run(() => ref = this.snackBar.openFromComponent(NotificationBarComponent, {
-        ...snackBarConfig,
-        data: {text: message},
-      }))
-    }
-
-    return ref
+    return this.show(successSnackBarConfig, message, duration)
   }
 
   public showError(message: string, duration?: number): MatSnackBarRef<SimpleSnackBar> {
-    const snackBarConfig = duration ? {...errorSnackBarConfig, duration} : errorSnackBarConfig
+    return this.show(errorSnackBarConfig, message, duration)
+  }
+
+  private show(defaultConfig: MatSnackBarConfig, message: string, duration?: number) {
+    const snackBarConfig: MatSnackBarConfig = duration ? {...defaultConfig, duration} : defaultConfig
     let ref
 
     if (message) {
