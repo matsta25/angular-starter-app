d55fa985db357cf629c39a9956cb13ab7e4e389e
cichy380 <marcin.dobroszek@gmail.com>
2020-05-11 12:19:22 +0200

    refactor(PostsApiService) Add POSTS_API_ENDPOINT

--- src/app/features/posts/services/posts-api.service.ts
+++ src/app/features/posts/services/posts-api.service.ts
@@ -5,6 +5,8 @@ import { Post } from '../models/post.model'
 import { Observable } from 'rxjs'
 import { ApiServiceModel } from '../../../shared/models/api-service.model'
 
+const POSTS_API_ENDPOINT = '/posts'
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,22 +15,22 @@ export class PostsApiService implements ApiServiceModel<Post> {
   }
 
   public createItem(post: Post): Observable<object> {
-    return this.http.post(`${environment.baseUrl}/posts`, post)
+    return this.http.post(`${environment.baseUrl}${POSTS_API_ENDPOINT}`, post)
   }
 
   public readItems(): Observable<object> {
-    return this.http.get(`${environment.baseUrl}/posts`)
+    return this.http.get(`${environment.baseUrl}${POSTS_API_ENDPOINT}`)
   }
 
   public readItem(id: string): Observable<object> {
-    return this.http.get(`${environment.baseUrl}/posts/${id}`)
+    return this.http.get(`${environment.baseUrl}${POSTS_API_ENDPOINT}/${id}`)
   }
 
   public updateItem(id: string, changes: Partial<Post>): Observable<object> {
-    return this.http.put(`${environment.baseUrl}/posts/${id}`, changes)
+    return this.http.put(`${environment.baseUrl}${POSTS_API_ENDPOINT}/${id}`, changes)
   }
 
   public deleteItem(id: string): Observable<object> {
-    return this.http.delete(`${environment.baseUrl}/posts/${id}`)
+    return this.http.delete(`${environment.baseUrl}${POSTS_API_ENDPOINT}/${id}`)
   }
 }
