7a49425dfcb7cec28f0d17fa9512fe64c220d6f0
cichy380 <marcin.dobroszek@gmail.com>
2020-05-12 08:10:48 +0200

    feat(ActionInterceptor) Add ActionInterceptor

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -1,6 +1,8 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { MessageInterceptor } from './message.interceptor'
+import { ActionInterceptor } from './action.interceptor'
 
 export const httpInterceptorProviders = [
   {provide: HTTP_INTERCEPTORS, useClass: MessageInterceptor, multi: true},
+  {provide: HTTP_INTERCEPTORS, useClass: ActionInterceptor, multi: true},
 ]
